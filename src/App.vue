9755221851a5c68322b98787d4f185946e69ae2f
JJJACY <49667416+JJJACY@users.noreply.github.com>
2021-05-17 15:58:29 +0800

    style: setup

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { useRouter } from 'vue-router'
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -13,6 +14,10 @@ export default defineComponent({
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  setup() {
+    const router = useRouter()
+    return { router }
   }
 });
 </script>
